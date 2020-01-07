4fa6d843c31af80969bd8b4613a9e8ecc9837c77
jopemachine <jopemachine@naver.com>
2020-01-07 22:17:35 +0900

    [feat] Add APILimiter

---
+++ src/apiLimiter.ts
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import RateLimit from "express-rate-limit";
+
+const rateLimit = new RateLimit({
+  windowMs: 1000,
+  max: 2,
+  delayMs: 0,
+  handler(req, res) {
+    res.status(this.statusCode).json({
+      code: this.statusCode,
+      message: '초당 2회까지만 요청할 수 있습니다.'
+    })
+  }
+});
+
+export default rateLimit;
