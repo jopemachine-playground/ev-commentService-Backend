99000be9889117f2af702baf808b36479c71dbec
jopemachine <jopemachine@naver.com>
2020-01-07 22:18:22 +0900

    [feat] Add authentification using webtoken
    해당 코드는 NodeJS 교과서를 참고

---
+++ src/authentification.ts
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+
+export function isSignedIn (req, res, next) {
+  if(req.isAuthenticated()){
+    next();
+  } else {
+    res.status(403).send('먼저 로그인을 해 주세요!');
+  }
+}
+
+export function isNotSignedIn (req, res, next) {
+  if(!req.isAuthenticated()){
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
+
+export function verifyToken (req, res, next) {
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch(error) {
+    if(error.name === 'TokenExpiredError') {
+      return res.status(419).json({
+        code: 419,
+        message: "토큰이 만료되었습니다."
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: "유효하지 않은 토큰입니다."
+    });
+  }
+}
+
+
+
